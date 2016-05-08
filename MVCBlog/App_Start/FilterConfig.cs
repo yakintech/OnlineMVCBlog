9efbf5383dbb0366fd0969721f9317cfae22f61f
Çağatay Yıldız <cagatay.yildiz@yakintech.com>
2016-05-08 11:52:17 +0300

    elmah

---
+++ MVCBlog/App_Start/FilterConfig.cs
@@ -0,0 +1,30 @@
+﻿using Elmah;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBlog.App_Start
+{
+    public class FilterConfig
+    {
+
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new ElmahHandledErrorLoggerFilter());
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public class ElmahHandledErrorLoggerFilter : System.Web.Mvc.IExceptionFilter
+        {
+            public void OnException(ExceptionContext context)
+            {
+                if (context.ExceptionHandled)
+                {
+                    ErrorSignal.FromCurrentContext().Raise(context.Exception);
+                }
+            }
+        }
+    }
+}
--- MVCBlog/Global.asax.cs
+++ MVCBlog/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MVCBlog.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace MVCBlog
@@ -11,6 +13,8 @@ namespace MVCBlog
     {
         protected void Application_Start()
         {
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
