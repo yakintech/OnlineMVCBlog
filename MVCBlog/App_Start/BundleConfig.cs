a40c498736e00dbe4cfd58959c1a8b8c0ef4b2c4
Çağatay Yıldız <cagatay.yildiz@yakintech.com>
2016-05-08 11:29:28 +0300

    add bundleconfig

---
+++ MVCBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace MVCBlog.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            bundles.Add(new StyleBundle("~/bundles/admincss"
+                        ).Include(
+                        "~/Areas/Admin/Content/css/AdminLTE.css",
+                        "~/Areas/Admin/Content/css/_all-skins.min.css"
+                        ));
+
+            bundles.Add(new ScriptBundle("~/bundles/adminjs").Include(
+                "~/Scripts/jquery.slimscroll.min.js",
+                "~/Scripts/fastclick.min.js",
+                "~/Scripts/app.min.js",
+                "~/Scripts/demo.js"
+                ));
+
+
+               BundleTable.EnableOptimizations = true;
+        }
+    }
+}
